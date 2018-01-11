d62d96cb19c58e1a02a41cb65aa5e731c961a797
Sancar Gazi <sancar.gazi@valtech.de>
2018-01-11 18:22:47 +0100

    allow tag when there is already a tag on HEAD

--- src/main/groovy/net/vivin/gradle/versioning/VersionUtils.groovy
+++ src/main/groovy/net/vivin/gradle/versioning/VersionUtils.groovy
@@ -109,9 +109,9 @@ class VersionUtils {
 
                 result = versionFromTags
             } else {
-                if(version.bump || version.newPreRelease || version.promoteToRelease) {
-                    throw new BuildException('Cannot bump the version, create a new pre-release version, or promote a pre-release version because HEAD is currently pointing to a tag that identifies an existing version. To be able to create a new version, you must make changes', null)
-                }
+//                if(version.bump || version.newPreRelease || version.promoteToRelease) {
+//                    throw new BuildException('Cannot bump the version, create a new pre-release version, or promote a pre-release version because HEAD is currently pointing to a tag that identifies an existing version. To be able to create a new version, you must make changes', null)
+//                }
 
                 version.snapshot = false
                 return headTag - TAG_PREFIX_PATTERN
